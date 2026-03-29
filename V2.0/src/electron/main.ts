a4d9ecdefc9c67eb988680d5111fac2167673abf
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-03-29 12:13:35 +0600

    working on new vartion

---
+++ V2.0/src/electron/main.ts
@@ -0,0 +1,11 @@
+import {app,BrowserWindow} from "electron";
+import path from "path"; // for work on windows and linux both
+
+// type test  = string;
+
+
+app.on("ready",()=>{
+    const mainWindow = new BrowserWindow({});
+    
+    mainWindow.loadFile(path.join( app.getAppPath() + '/out/index.html'));
+})
--- dist-electron/main.js
+++
@@ -1,7 +0,0 @@
-import { app, BrowserWindow } from "electron";
-import path from "path"; // for work on windows and linux both
-// type test  = string;
-app.on("ready", () => {
-    const mainWindow = new BrowserWindow({});
-    mainWindow.loadFile(path.join(app.getAppPath() + '/out/index.html'));
-});
