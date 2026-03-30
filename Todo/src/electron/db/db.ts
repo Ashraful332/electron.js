665f1004363d61d796ce1224f5e186f16f2e116d
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-03-30 13:34:47 +0600

    app is working

--- Todo/src/electron/db/db.ts
+++ Todo/src/electron/db/db.ts
@@ -1,9 +1,13 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+import path from "path";
+import { app } from "electron";
 
 export const initDB = async () => {
+    const dbPath = path.join(app.getPath("userData"), "todo.db");
+
     const db = await open({
-        filename: "./todo.db",
+        filename: dbPath,
         driver: sqlite3.Database,
     });
 
